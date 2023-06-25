7040ba776c737a1ae6bdcd297ffe9b829e3818e2
itani <itani@DESKTOP-N25EOF2>
2023-06-25 13:10:49 -0700

    Add project files.

---
+++ insert/insert.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+//insertion sort
+
+int main()
+{
+    int length;
+    int num;
+    cout << "enter length of list: ";
+    cin >> length;
+    vector<int> arr(length, 0);
+    for (int i = 0; i < length; i++) {
+        cout << "enter number: ";
+        cin >> num;
+        arr[i] = num;
+    }
+
+    for (int i = 1; i < length; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && key < arr[j]) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+    for (int i = 0; i < length; i++) {
+        cout << arr[i];
+    }
+
+    return 0;
+}
